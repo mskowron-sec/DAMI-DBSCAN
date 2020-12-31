92dd9afb88240df30a237ad004c346678887927f
Monika Skowron <m0nia@cyber.lab>
2020-12-31 13:19:03 +0100

    Dodaj pliki projektów.

---
+++ DAMI-DBSCAN/DAMI-DBSCAN.cpp
@@ -0,0 +1,30 @@
+﻿// DAMI-DBSCAN.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <vector>
+@Point - 
+class Point {
+public:
+    double x, y;
+    int noOfPoints;
+    int eps;
+    double getDistance(const Point& point) {
+        return sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
