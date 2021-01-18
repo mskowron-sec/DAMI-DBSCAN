f7e503afa024c05ff201c0c9fc213c34fd0402af
purpl3H4t <monika8973@gmail.com>
2021-01-18 12:25:57 +0100

    read  files

--- DAMI-DBSCAN/DAMI-DBSCAN.cpp
+++ DAMI-DBSCAN/DAMI-DBSCAN.cpp
@@ -3,19 +3,70 @@
 
 #include <iostream>
 #include <vector>
-@Point - 
+#include <fstream>
+#include <string>
+using namespace std;
+int status;
+const int NOT_CLASSIFIED = -1;
 class Point {
 public:
     double x, y;
     int noOfPoints;
-    int eps;
+    int clusterNo;
+    int label;
     double getDistance(const Point& point) {
         return sqrt((x - point.x) * (x - point.x) + (y - point.y) * (y - point.y));
     }
 };
+class InputReader {
+private:
+    ifstream fin;
+    vector<Point> points;
+    enum Status { NOT_VISITED = -1, BORDER, NOISE, CORE };
+public:
+    InputReader(string filename) {
+        fin.open(filename);
+        if (!fin) {
+            cout << filename << "Error!\n";
+            exit(0);
+        }
+        parse();
+    }
+    void parse() {
+        int label;
+        double x, y;
+        string element;
+        while (!fin.eof()) {
+            //fin >> x>>" ">>y>>"," >> label;
+            while (getline(fin, element, ','))
+            {
+               
+            }
+            points.push_back({ x,y,0, NOT_CLASSIFIED, label});
+        }
+        points.pop_back();
+         
+         
+        for (auto i = points.begin(); i != points.end(); i++)
+        {
+            cout << i->x<<' '<< i->y <<' ' <<i->label << endl;
+
+        }
+    }
+    vector<Point> getPoints() {
+        return points;
+    }
+};
+//Point class - 2D
+//TODO N-Dimensions
+
 int main()
 {
     std::cout << "Hello World!\n";
+    std::string path = "test.arff";
+    InputReader input = InputReader(path);
+   // std::cout << input.getPoints()
+    
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
